b9a37ddd390bc0a148585a4136c0a51c5809e1d0
lschachter <lillieschachter@gmail.com>
2020-03-13 23:38:32 -0400

    Update dependencies

--- src/utilities/search.js
+++ src/utilities/search.js
@@ -4,9 +4,14 @@
 export const search = async (topic, startIndex) => {
 	let baseUrl = "https://www.googleapis.com/books/v1/volumes?q=";
   const apiUrl = baseUrl + topic + "&startIndex=" + startIndex;
-  const response = await fetch(apiUrl);
-  const booksJson = await response.json();
-  if (response.status !== 200) throw Error(booksJson.message);
-  // api still returns json item on no data
-  return booksJson;
+  try {
+    const response = await fetch(apiUrl);
+    const booksJson = await response.json();
+    if (response.status !== 200) throw Error(booksJson.message);
+    // api still returns json object on no data
+    return booksJson;
+  } catch(err) {
+    console.log(err);
+    return JSON.stringify({});
+  }
 }
